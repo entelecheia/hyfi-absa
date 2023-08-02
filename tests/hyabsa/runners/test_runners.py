24b0be587eb190249a9457658431c151d2d1563a
Young Joon Lee <entelecheia@hotmail.com>
2023-08-02 21:20:44 +0900

    refactor(test_runners): remove print statements

--- tests/hyabsa/runners/test_runners.py
+++ tests/hyabsa/runners/test_runners.py
@@ -4,14 +4,12 @@ from hyabsa.runners import AbsaRunner
 def test_absa_runner():
     runner = AbsaRunner()
     assert runner is not None
-    print(AbsaRunner.generate_config())
+    AbsaRunner.generate_config()
 
 
 def test_absa_runner_execute():
     runner = AbsaRunner(_config_name_="test")
     assert runner is not None
-    args = runner.data_load.config
-    print(args, type(args))
     print(runner.run())
 
 
